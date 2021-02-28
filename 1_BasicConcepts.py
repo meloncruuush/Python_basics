9fa31f91e6e6b473c2b6abde030afec947594d5d
meloncrush <matteobecatti7@gmail.com>
2021-02-28 13:41:33 +0100

    prova

---
+++ 1_BasicConcepts.py
@@ -0,0 +1,21 @@
+#this is how you make comments
+
+print("This is an output.\nOperation examples: ", 1+1, 1*5, 6-3, 6/2, 4**2, 10//3, 10%3, "\n")
+
+print("""this 
+is a 
+multiline
+text""")
+
+print("\nthis is a " + "concatenation")
+
+print("\nsend help"*3)
+
+pin = "1234"
+
+username = input("Choose your username: ")
+print("Nice username, " + username)
+
+#you can convert with int(), str(), float()
+
+age = int(input("\nHow old are you? ")) 
