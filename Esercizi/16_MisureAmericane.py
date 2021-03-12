e1e2e4bd975657ac3cfed71bf1b9ac94b6c05c78
meloncrush <matteobecatti7@gmail.com>
2021-03-12 16:50:01 +0100

    finish ex 16

--- Esercizi/16_MisureAmericane.py
+++ Esercizi/16_MisureAmericane.py
@@ -1,13 +1,17 @@
 # Scrivi una funzione che, dato in ingresso un valore espresso in metri, mandi in print 
 # l'equivalente in miglia terrestri, iarde, piedi e pollici.
 
-def conversione_americana(m):
-    miglia = m/0.000621371
-    iarde = m*1.09361
-    piedi = m*3,28084
-    pollici = m*39,2701
-    print(str(m) + "metri equivalgono a " + str(miglia) + " miglia, " + str(iarde) + " iarde, " + str(piedi) + " piedi, " + str(pollici), " pollici.")
+
+def conversione_americana(metri):
+    valori_convertiti = {
+        "miglia": metri * 0.000621371,
+        "iarde": metri * 1.09361,
+        "piedi": metri * 3.28084,
+        "pollici": metri * 39.2701}
+
+    print(f"{metri} metri corrispondono a:")
+    for key, value in valori_convertiti.items():
+        print(f"{key}: {value}")
 
 
-
-conversione_americana(1)
+conversione_americana(10)
