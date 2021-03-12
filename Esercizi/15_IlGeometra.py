67835873408e06ac215ac667b7dc15511d6ef526
meloncrush <matteobecatti7@gmail.com>
2021-03-12 12:12:56 +0100

    piccoli progressi, inizio transizione a PyCharm

--- Esercizi/15_IlGeometra.py
+++ Esercizi/15_IlGeometra.py
@@ -1,28 +1,42 @@
 #Scrivi una funzione che, a scelta dell'utente, calcoli l'area di: un cerchio, un quadrato, un rettangolo, un triangolo 
 
-def calcolaArea():
+from math import pi
+
+def calcola_area():
     switch = {
-        1: areaCerchio,
-        2: areaQuadrato,
-        3: areaRettangolo,
-        4: areaTriangolo
+        1: area_cerchio,
+        2: area_quadrato,
+        3: area_rettangolo,
+        4: area_triangolo
     }
 
-    x = int( input("Quale area vuoi calcolare?\n1)Cerchio\n2)Quadrato\n3)Rettangolo\n4)Triangolo\nInserisci: ") )
+    x = int( input("Quale area vuoi calcolare?\n1)Cerchio\n2)Quadrato\n3)Rettangolo\n4)Triangolo\n\nInserisci: ") )
     
     funzione = switch.get(x, "Input non valido")
-    print(funzione())
+    funzione()
 
-def areaCerchio():
-    print("area cercio")
+def area_cerchio():
+    r = float(input("Inserisci il raggio: "))
+    area = (r**2)*pi
+    print(round(area,3))
 
-def areaQuadrato():
-    print("quadra")
+def area_quadrato():
+    lato = float(input("Inserisci il lato del quadrato: "))
+    area = lato**2
+    print(round(area, 2))
 
-def areaRettangolo():
-    print("rett")
+def area_rettangolo():
+    base, altezza = float(input("Inserisci la base: ")), float(input("Inserisci l'altezza: "))
+    area = base*altezza
+    print(round(area, 2))
 
-def areaTriangolo():
-    print("tri")
+def area_triangolo():
+    base, altezza = float(input("Inserisci la base: ")), float(input("Inserisci l'altezza: "))
+    area = base*altezza/2
+    print(round(area, 2))
 
-calcolaArea()
+while True:
+    calcola_area()
+    scelta = int(input("\n\nVuoi continuare?\n1)Sì\n0)Esci\n"))
+    if scelta == 0:
+        break
